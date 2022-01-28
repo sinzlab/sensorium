7a37fb4291563aa2ea7405b86d81bb87aeafef47
Mohammad Bashiri <mohammadbashiri93@gmail.com>
2022-01-28 16:35:12 +0100

    Remove the CLI portion from the eval.py

--- cascade/eval.py
+++ cascade/eval.py
@@ -1,15 +1,23 @@
-import os
-import click
-import json
 import ast
 import numpy as np
 import pandas as pd
 from cascade.utility.metrics import Metrics
 
-FOLDER = "output"
-
 
 def load_submission_data(submission_path):
+    """
+    Extracts necessary data for model evaluation from the submitted csv file.
+
+    Args:
+        submission_path (str): Complete path to the submission file.
+
+    Returns:
+        tuple: Contains:
+               - trial indices (1D array)
+               - image IDs (1D array)
+               - neuron IDs (1D array)
+               - predictions (2d array: trials x neurons)
+    """
     submission_df = pd.read_csv(submission_path)
     trial_idx = submission_df["trial_indices"].values
     image_ids = submission_df["image_ids"].values
@@ -21,19 +29,34 @@ def load_submission_data(submission_path):
     return trial_idx, image_ids, neuron_ids, predictions
 
 
-def load_groundtruth_data(submission_path):
-    submission_df = pd.read_csv(submission_path)
-    trial_idx = submission_df["trial_indices"].values
-    image_ids = submission_df["image_ids"].values
-    neuron_ids = np.array(ast.literal_eval(submission_df["neuron_ids"].values[0]))
-    predictions = np.array(
-        [ast.literal_eval(v) for v in submission_df["prediction"].values]
-    )
+def load_groundtruth_data(groundtruth_path):
+    """
+    Extracts necessary data for model evaluation from the ground truth data file.
 
-    return trial_idx, image_ids, neuron_ids, predictions
+    Args:
+        groundtruth_path (str): Complete path to the ground truth data file.
+
+    Returns:
+        tuple: Contains:
+               - trial indices (1D array)
+               - image IDs (1D array)
+               - neuron IDs (1D array)
+               - predictions (2d array: trials x neurons)
+    """
+    raise NotImplementedError()
 
 
 def evaluate(submission_path, ground_truth_path):
+    """
+    Compute evaluation metrics for a specific submission given the ground truth data.
+
+    Args:
+        submission_path (str): Complete path to the submission csv file.
+        ground_truth_path (str): Complete path to the ground truth data file.
+
+    Returns:
+        dict: Containing all the evaluation results for all the evaluation metrics.
+    """
     trial_idx_gt, image_ids_gt, neuron_ids_gt, responses = load_groundtruth_data(
         ground_truth_path
     )
@@ -59,38 +82,3 @@ def evaluate(submission_path, ground_truth_path):
         predictions, trial_idx_submitted, neuron_ids_submitted, per_neuron=False
     )
     return output
-
-
-@click.command()
-@click.option(
-    "--submission_path",
-    prompt="The submission_path",
-    help="Provide the path where the submission is placed",
-)
-@click.option(
-    "--ground_truth_path",
-    prompt="Provide the path where the ground_truth is placed",
-    help="The ground_truth_path",
-)
-@click.option(
-    "--submission_id",
-    prompt="Provide submission id",
-    help="The submission_id to create a result",
-)
-def main(submission_path, ground_truth_path, submission_id):
-    """ Templated execution file"""
-    try:
-        output = evaluate(submission_path, ground_truth_path)
-        print(f"The evaluation output:{output}")
-        if not os.path.exists(FOLDER):
-            os.makedirs(FOLDER)
-        with open(os.path.join(FOLDER, submission_id + ".json"), "w") as f:
-            json.dump(output, f)
-        print("Success:Completed the evaluation written to output dir")
-    except Exception as e:
-        print(e)
-        print("Exception:There has been an error while executing file")
-
-
-if __name__ == "__main__":
-    main()
