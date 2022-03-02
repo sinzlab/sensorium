ea82b6be4e257107e7bffb286003c1b1821bc356
KonstantinWilleke <konstantin.willeke@gmail.com>
2022-03-02 09:11:34 +0100

    restructure submission.py to work with FileTree and Hub loaders.

--- cascade/utility/submission.py
+++ cascade/utility/submission.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import torch
+import numpy as np
+
+from nnfabrik.builder import get_data
 from neuralpredictors.training import eval_state, device_state
+from neuralpredictors.data.datasets import FileTreeDataset
+
 
 # from cascade.utility.scores import model_predictions # why does this return targets?
 def model_predictions(model, dataloader, data_key, device="cpu"):
@@ -25,15 +30,83 @@ def model_predictions(model, dataloader, data_key, device="cpu"):
     return output.numpy()
 
 
-def generate_submission_file(trained_model, test_dataloader, data_key=None, path=None):
+def get_data_filetree_loader(filename=None, dataloader=None):
+    """
+    Extracts necessary data for model evaluation from a dataloader based on the FileTree dataset.
+
+    Args:
+        filename (str): Specifies a path to the FileTree dataset.
+        dataloader (obj): PyTorch Dataloader
+
+    Returns:
+        tuple: Contains:
+               - trial indices (1D array)
+               - image IDs (1D array)
+               - neuron IDs (1D array)
+               - responses (2d array: trials x neurons)
+    """
+
+    if dataloader is None:
+        dataset_fn = "cascade.datasets.static_loaders"
+        dataset_config = {
+            "paths": filename,
+            "normalize": False,
+            "batch_size": 64,
+        }
+        dataloaders = get_data(dataset_fn, dataset_config)
+        data_key = list(dataloaders["test"].keys())[0]
+
+        dat = dataloaders["train"][data_key].dataset
+    else:
+        dat = dataloader.dataset
+
+    neuron_ids = dat.neurons.unit_ids.tolist()
+    tiers = dat.trial_info.tiers
+    complete_image_ids = dat.trial_info.frame_image_id
+    complete_trial_idx = dat.trial_info.trial_idx
+
+    trial_indices, responses, image_ids = [], [], []
+    for i, datapoint in enumerate(dat):
+        if tiers[i] != "test":
+            continue
+
+        trial_indices.append(complete_trial_idx[i])
+        image_ids.append(complete_image_ids[i])
+        responses.append(datapoint.responses.cpu().numpy().squeeze())
+
+    #trial_indices = np.array(trial_indices)
+    #image_ids = np.array(image_ids)
+    responses = np.stack(responses)
+
+    return trial_indices, image_ids, neuron_ids, responses
+
+
+def get_data_hub_loader(dataloader):
+    image_ids = dataloader.dataset.dataset.image_ids.data().flatten().tolist()
+    trial_indices = dataloader.dataset.dataset.trial_indices.data().flatten().tolist()
+    neuron_ids = dataloader.dataset.dataset.info["neuron_ids"]
+
+    return trial_indices, image_ids, neuron_ids
+
+
+def generate_submission_file(
+    trained_model, test_dataloader, data_key=None, path=None, device="cpu"
+):
     test_predictions = model_predictions(
-        trained_model, test_dataloader, data_key=data_key
+        trained_model,
+        test_dataloader,
+        data_key=data_key,
+        device=device,
     )
-    image_ids = test_dataloader.dataset.dataset.image_ids.data().flatten().tolist()
-    trial_indices = (
-        test_dataloader.dataset.dataset.trial_indices.data().flatten().tolist()
-    )
-    neuron_ids = test_dataloader.dataset.dataset.info["neuron_ids"]
+
+    if issubclass(type(test_dataloader.dataset), FileTreeDataset):
+        trial_indices, image_ids, neuron_ids, _ = get_data_filetree_loader(
+            dataloader=test_dataloader
+        )
+    else:
+        trial_indices, image_ids, neuron_ids = get_data_hub_loader(
+            dataloader=test_dataloader
+        )
 
     df = pd.DataFrame(
         {
@@ -46,5 +119,37 @@ def generate_submission_file(trained_model, test_dataloader, data_key=None, path
 
     path = path if path is not None else "submission_file.csv"
     df.to_csv(path, index=False)
-
     print("File saved.")
+
+
+def generate_ground_truth_file(
+    filename,
+    path=None,
+):
+    """
+    Extract necessary data for model evaluation from the ground truth data file.
+
+    Args:
+        filename (str): Specifies which of benchmark datasets to get the ground truth data from.
+
+    Returns:
+        tuple: Contains:
+               - trial indices (1D array)
+               - image IDs (1D array)
+               - neuron IDs (1D array)
+               - responses (2d array: trials x neurons)
+    """
+    trial_indices, image_ids, neuron_ids, responses = get_data_filetree_loader(
+        filename=filename
+    )
+    df = pd.DataFrame(
+        {
+            "trial_indices": trial_indices,
+            "image_ids": image_ids,
+            "responses": responses.tolist(),
+            "neuron_ids": [neuron_ids] * len(responses),
+        }
+    )
+
+    path = path if path is not None else "ground_truth_file.csv"
+    df.to_csv(path, index=False)
