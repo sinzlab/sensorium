a6e34f797bbc6d436ff0b644ef2db6ad37dbbe24
KonstantinWilleke <konstantin.willeke@gmail.com>
2022-06-19 08:40:20 +0200

    reemove legacy measure helpers.

--- sensorium/utility/measure_helpers.py
+++ sensorium/utility/measure_helpers.py
@@ -1,38 +1,11 @@
-import types
-import warnings
-import numpy as np
-import math
+import pandas as pd
 
 
-def get_subset_of_repeats(outputs, repeat_limit, randomize=True):
-    """
-    Args:
-        outputs (array or list): repeated responses/targets to the same input. with the shape [inputs, ] [reps, neurons]
-                                    or array(inputs, reps, neurons)
-        repeat_limit (int): how many reps are selected
-        randomize (cool): if True, takes a random selection of repetitions. if false, takes the first n repetitions.
-
-    Returns: limited_outputs (list): same shape as inputs, but with reduced number of repetitions
-
-    """
-    limited_output = []
-    for repetitions in outputs:
-        n_repeats = repetitions.shape[0]
-        limited_output.append(
-            repetitions[
-                :repeat_limit,
-            ]
-            if not randomize
-            else repetitions[
-                np.random.choice(
-                    n_repeats,
-                    repeat_limit if repeat_limit < n_repeats else n_repeats,
-                    replace=False,
-                )
-            ]
-        )
-    return limited_output
-
-
-def is_ensemble_function(model):
-    return isinstance(model, types.FunctionType)
+def get_df_for_scores(session_dict, measure_attribute='score'):
+    data_keys, values = [], []
+    for key, unit_array in session_dict.items():
+        for value in unit_array:
+            data_keys.append(key)
+            values.append(value)
+    df = pd.DataFrame({'dataset':data_keys, measure_attribute:values})
+    return df
